54bb1ad197d587361d2b58fa0a4cfde1aa14e73d
turbopope <PapstDonB@Googlemail.com>
2019-08-04 18:07:17 +0800

    Move tick logic into reducer

--- src/components/World.js
+++ src/components/World.js
@@ -2,31 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Row from './Row';
 import './World.css';
+import { tick } from '../actions/index';
 
 class World extends React.Component {
-
-  // componentDidMount = () => {
-  //   this.randomizeWorld();
-  //   this.interval = setInterval(this.tick, this.props.tickDuration);
-  // }
-
-  // componentWillUnmount = () => {
-  //   clearInterval(this.interval);
-  // }
-
-  // componentWillUpdate = () => {
-  //   clearInterval(this.interval);
-  //   this.interval = setInterval(this.tick, this.props.tickDuration);
-  // }
-
-  // tick = () => {
-  //   if (!this.props.paused) {
-  //     let oldWorld = this.state.world;
-  //     let newWorld = Game.tick(oldWorld);
-  //     this.setState({world: newWorld});
-  //   }
-  // }
-
   render = () => {
     return <div className="World">
       {this.props.world.map((row, index) => <Row key={index} row={index} cells={row}></Row>)}
@@ -36,4 +14,8 @@ class World extends React.Component {
 
 const mapStateToProps = state => ({...state});
 
-export default connect(mapStateToProps)(World);
+const mapDispatchToProps = dispatch => ({
+  tick: () => dispatch(tick())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(World);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { TOGGLE_PAUSED, SET_TICK_DURATION, TICK, RANDOMIZE, CLEAR, TOGGLE_CELL } from "../actions/action-types";
 import Game from "../gameOfLife";
+import store from '../store/index';
+import { tick } from '../actions/index';
 
 const WORLD_WIDTH = 32;
 const WORLD_HEIGHT = 48;
@@ -7,12 +9,24 @@ const WORLD_HEIGHT = 48;
 const initialState = {
   paused: false,
   tickDuration: 100,
-  world: Game.newRandomWorld(WORLD_WIDTH, WORLD_HEIGHT)
+  world: Game.newRandomWorld(WORLD_WIDTH, WORLD_HEIGHT),
 };
+initialState.tickInterval = setInterval(() => store.dispatch(tick()), initialState.tickDuration);
+
 function rootReducer(state = initialState, action) {
+  console.log(action)
   if (action.type === TOGGLE_PAUSED)     return { ...state, paused: !state.paused };
-  if (action.type === SET_TICK_DURATION) return { ...state, tickDuration: action.payload };
-  if (action.type === TICK)              return { ...state, world: Game.tick(state.world) };
+
+  if (action.type === SET_TICK_DURATION) {
+    clearInterval(state.tickInterval);
+    return { 
+      ...state,
+      tickDuration: action.payload,
+      tickInterval: setInterval(() => store.dispatch(tick()), action.payload)
+    };
+  }
+
+  if (action.type === TICK)              return { ...state, world: state.paused ? state.world : Game.tick(state.world) };
   if (action.type === RANDOMIZE)         return { ...state, world: Game.newRandomWorld(WORLD_WIDTH, WORLD_HEIGHT) };
   if (action.type === CLEAR)             return { ...state, world: Game.newEmptyWorld(WORLD_WIDTH, WORLD_HEIGHT) };
   if (action.type === TOGGLE_CELL)       return { ...state, world: Game.toggleCell(state.world, action.payload.row, action.payload.col) };
